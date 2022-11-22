9fb2bdf3dc7a55ae10371229188391e5bdfc2525
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-11-21 23:49:22 -0300

    fix(ui): remove hardcoded dimension values

--- app/routes/__dapp/index.tsx
+++ app/routes/__dapp/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    <div className="relative flex h-[400px] w-[800px] items-center justify-center overflow-hidden">
+    <div className="relative flex items-center justify-center overflow-hidden">
       <Link to="showcase">
         <button className="btn-primary">See showcase</button>
       </Link>
